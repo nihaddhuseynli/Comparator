83c231713b1439d53f2ad75b8df75065b557c34f
nihaddhuseynli <104736036+nihaddhuseynli@users.noreply.github.com>
2025-03-15 23:35:06 +0400

    Delete Comparator1.java

--- Comparator1.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class Comparator1 implements Comparator<Student> {
-    @Override
-    public int compare(Student s1, Student s2) {
-        return Integer.compare(s1.getId(), s2.getId());
-    }
-}
