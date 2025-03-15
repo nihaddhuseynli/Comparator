f9746d0e92692fa9022235fc0d051e344b5fbd8b
nihaddhuseynli <104736036+nihaddhuseynli@users.noreply.github.com>
2025-03-15 23:31:33 +0400

    Update Comparator1.java

--- Comparator1.java
+++ Comparator1.java
@@ -3,6 +3,6 @@ import java.util.Comparator;
 public class Comparator1 implements Comparator<Student> {
     @Override
     public int compare(Student s1, Student s2) {
-        return Double.compare(s2.getGrade(), s1.getGrade());
+        return Integer.compare(s1.getId(), s2.getId());
     }
 }
